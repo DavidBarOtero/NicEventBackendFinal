2288b4e6e9a24bf128d665b7818ebf127f65f57b
DaviBarOtero <davidbarotero@gmail.com>
2020-01-17 15:52:10 +0100

    solucionado problema con props email sendgrid

--- app/webServer/controllers/Payments/event-payment.js
+++ app/webServer/controllers/Payments/event-payment.js
@@ -4,13 +4,13 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRIPKEY);
 
 async function payment(req, res, next) {
-  const professionalName = req.body.data.name;
-  const professionalEmail = req.body.data.email;
-  console.log(professionalEmail);
+  const professionalName = req.body.name;
+  const professionalEmail = req.body.email;
   console.log(professionalName);
+  console.log(professionalEmail);
   const msg = {
     to: `${professionalEmail}`,
-
+    // to: "davidbarotero@gmail.com",
     from: "NiceEvent@gmail.com",
     subject: "Nuevo evento cerrado",
     text: `¡¡Enhorabuena!! ${professionalName} has sido elegido para un evento en la siguiente fecha: `
